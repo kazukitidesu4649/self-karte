8237c8a0034c626b2fefb8e35de3fee0bc6d5da2
kazukitidesu4649 <kazukitidesu@gmail.com>
2026-05-20 23:18:10 +0900

    シーダーの途中、 歯医者のカテゴリと条件の続きから

--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $major_category_names = [
+            '美容院', '歯医者', '内科'
+        ];
+
+        $biyoin_categories = [
+            '髪の長さ', 'パーマ', '染織'
+        ];
+
+        $haisya_categoriees = [
+            '体温', '症状', '薬'
+        ];
+
+        foreach ($major_category_names as $major_category_name) {
+            if ($major_category_name == '美容院') {
+                foreach ($biyoin_categories as $biyoin_categorie) {
+                    category::create([
+                        'name' => $biyoin_categorie,
+                        'description' => $biyoin_categorie,
+                        'major_category_name' => $major_category_name
+                    ]);
+                }
+            }
+
+            if () {}
+            if () {}
+        }
     }
 }
